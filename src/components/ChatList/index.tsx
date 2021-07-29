f211c70757c591b711b0cbc674452cc64300595d
Thamyris <thamyrissatana3@gmail.com>
2021-07-29 01:37:33 -0300

    updates

--- src/components/ChatList/index.tsx
+++ src/components/ChatList/index.tsx
@@ -7,30 +7,30 @@ import Chat from "./chat";
 const ChannelList: React.FC = () => {
   return (
     <Container>
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
-      <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
-      <Chat name="Lita" message="Fiz um desenho irado" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
       <Chat name="Lita" message="Fiz um desenho irado" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
       <Chat name="Lita" message="Fiz um desenho irado" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
       <Chat name="Lita" message="Fiz um desenho irado" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
       <Chat name="Lita" message="Fiz um desenho irado" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
       <Chat name="Lita" message="Fiz um desenho irado" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
       <Chat name="Lita" message="Fiz um desenho irado" />
-      <Chat name="Amor" message="Dá uma olhada, Gatinha" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
+      <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
+      <Chat name="Lita" message="Fiz um desenho irado" />
+      <Chat name="Amor" message="Dá uma olhada, amor" />
       <Chat name="Mãe" message="Filha, essa roupa é sua cara" />
       <Chat name="Lita" message="Fiz um desenho irado" />
     </Container>
